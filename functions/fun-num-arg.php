5a79f9ff61496552838f19f48d83224bb64478bf
dev-cyriacathickal <dev.cyriacathickal@gmail.com>
2024-11-02 23:03:55 +0530

    function num args updated

--- functions/fun-num-arg.php
+++ functions/fun-num-arg.php
@@ -2,8 +2,9 @@
 
 function numArg(){
     $args_number = func_get_args();
+    echo 'Total arguments: '. func_num_args(). "\n";
     if ($args_number > 2):
-        echo func_get_arg(3);
+        echo 'Position value is: '. func_get_arg(3);
     endif;
 }
 
