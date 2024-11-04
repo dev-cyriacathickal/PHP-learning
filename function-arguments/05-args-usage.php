6e3065fdb61929732e6f3cbcd85e63bfc44abb28
dev-cyriacathickal <dev.cyriacathickal@gmail.com>
2024-11-04 22:40:54 +0530

    function args usage added

---
+++ function-arguments/05-args-usage.php
@@ -0,0 +1,6 @@
+<?php
+function properFun($param1 = 'test1', $param2 = 'test2'){
+    return "This is param 1 : $param1 " . "and this is param 2 $param2";
+}
+
+var_dump(properFun(param2: 'test success'));
