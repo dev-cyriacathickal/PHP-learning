fc5454b220b7bf336df749f18205cb81ffb7ce90
dev-cyriacathickal <dev.cyriacathickal@gmail.com>
2024-11-04 22:41:50 +0530

    param 2 update added

--- function-arguments/05-args-usage.php
+++ function-arguments/05-args-usage.php
@@ -3,4 +3,4 @@ function properFun($param1 = 'test1', $param2 = 'test2'){
     return "This is param 1 : $param1 " . "and this is param 2 $param2";
 }
 
-var_dump(properFun(param2: 'test success'));
+var_dump(properFun(param2: 'test success'));//param 2 is updated here
