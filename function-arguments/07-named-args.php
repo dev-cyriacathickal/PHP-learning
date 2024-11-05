37d3de98922ef46cb49b0d7d6b44d66c73db3653
dev-cyriacathickal <dev.cyriacathickal@gmail.com>
2024-11-05 19:34:27 +0530

    named args sample aded

--- function-arguments/07-named-args.php
+++ function-arguments/07-named-args.php
@@ -1,1 +1,15 @@
 <?php
+
+/**
+ * 
+ * positional args vs named args
+ * 
+ * //positional
+ * funSample(1,44,66);
+ * 
+ * //named
+ * funsample(start_value:1, prim_value:44, secon_value:66);
+ * 
+ * 
+ * 
+ */
